f6a354a98b40db8499747c30672f2c4894a6c700
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-09-18 14:33:04 +0300

    Hot fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # SSH client configuration
 exec {'configure_ssh':
-command => "echo 'PasswordAuthentication no\nChallengeResponseAuthentication no'"\
+command => "echo -e 'PasswordAuthentication no\nChallengeResponseAuthentication no'"\
 "'\nUsePAM no\nIdentifyFile ~/.ssh/school' >> /etc/ssh/ssh_config"
 path    => '/bin/'
 }
