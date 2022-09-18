9b68909cb717a2a6ca3eadff2230e80ed83a800a
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-09-18 14:15:12 +0300

    Hot fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # SSH client configuration
 exec {'configure_ssh':
 command => 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config'
-path    => '/bin'
+path    => '/bin/'
 }
